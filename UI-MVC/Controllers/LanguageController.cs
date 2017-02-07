f4f1205e78fb4a4e19d149cbff870afc00b5c3ab
SeppeNoScope360 <seppe.demeester@student.kdg.be>
2017-02-07 17:14:53 +0100

    Moet comitten

---
+++ UI-MVC/Controllers/LanguageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SC.UI.Web.MVC.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+       /*public ActionResult SetLanguage(string name)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(name);
+            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+
+            HttpContext.Current.Session["culture"] = name;
+        }*/
+    }
+}
